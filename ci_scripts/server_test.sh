6d1effc25d9ea94331ac87403c002e3e007534de
Danny Joyce and Jason Rimer <pair+djoyce+jrimer@pivotal.io>
2017-11-13 09:36:25 -0500

    Combined Concourse jobs into a single job with tasks.

--- ci_scripts/server_test.sh
+++ ci_scripts/server_test.sh
@@ -4,9 +4,17 @@ set -o nounset
 set -o xtrace
 set -o pipefail
 
+export PATH="${PATH}:/root/.yarn/bin"
 export GRADLE_USER_HOME="${PWD}/gradle"
 
 pushd repo
 
+pushd client
+yarn install --no-progress
+yarn build
+popd
+
 # ./gradlew flywayMigrate
 ./gradlew test
+
+popd
