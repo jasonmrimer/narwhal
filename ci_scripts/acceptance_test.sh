01719a7d3d8f1870b246b4324d8b60406f73a15c
Danny Joyce and Jason Rimer <pair+djoyce+jrimer@pivotal.io>
2017-11-30 17:11:44 -0500

    WIP - attempt to fix selenium ipv6 issues.

--- ci_scripts/acceptance_test.sh
+++ ci_scripts/acceptance_test.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# Remove the ipv6 mapping for localhost so that selenium can resolve correctly
+cp /etc/hosts ~/hosts.new
+sed -i 's/::1/# ::1/g' ~/hosts.new
+cp -f ~/hosts.new /etc/hosts
+
 set -e
 
 until curl ${NARWHAL_APP_URL:='http://localhost:8080'} &>/dev/null; do
