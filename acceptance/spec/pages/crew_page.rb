ceba0c0a17b3aedb29bbd9c2c9eb64be70dadf2f
Danny Joyce and Jason Rimer <pair+djoyce+jrimer@pivotal.io>
2018-02-21 09:17:54 -0500

    Add js click to crew page.

--- acceptance/spec/pages/crew_page.rb
+++ acceptance/spec/pages/crew_page.rb
@@ -6,12 +6,20 @@ class CrewPage
     page.within('.side-panel') do
       find('a', text: 'AVAILABILITY').click
       page.within('.event-title', text: msn_title) do
-        find('a').click
+        click(find('a'))
       end
     end
   end
 
   def assert_has_assigned_airmen(*airmen)
     airmen.each { |airman| expect(page).to have_content(airman) }
+  end
+
+  def click(element)
+    script = <<-JS
+      arguments[0].click();
+    JS
+
+    Capybara.current_session.driver.browser.execute_script(script, element.native)
   end
 end
