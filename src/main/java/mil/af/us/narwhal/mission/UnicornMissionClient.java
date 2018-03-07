e176919fa65be322218947bac3661572ce3e5224
Jason Rimer <jrimer@pivotal.io>
2018-03-07 12:21:16 -0500

    unicorn mission service uses dynamic dates

--- src/main/java/mil/af/us/narwhal/mission/UnicornMissionClient.java
+++ src/main/java/mil/af/us/narwhal/mission/UnicornMissionClient.java
@@ -7,6 +7,8 @@ import org.springframework.ws.soap.client.core.SoapActionCallback;
 import unicorn.GetMissionMetaDataRestV2;
 import unicorn.GetMissionMetaDataRestV2Response;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 public class UnicornMissionClient extends WebServiceGatewaySupport implements MissionClient {
@@ -27,9 +29,15 @@ public class UnicornMissionClient extends WebServiceGatewaySupport implements Mi
 
   @Override
   public List<Results.MissionMetaData> getMissionMetaData() {
+    SimpleDateFormat format1 = new SimpleDateFormat("MM/dd/yyyy");
+    Calendar cal = Calendar.getInstance();
+    String startDate = format1.format(cal.getTime());
+    cal.add(Calendar.DAY_OF_YEAR, 14);
+    String endDate = format1.format(cal.getTime());
+
     GetMissionMetaDataRestV2 getMissionMetaData = new GetMissionMetaDataRestV2();
-    getMissionMetaData.setStart("03/01/2018");
-    getMissionMetaData.setEnd("04/01/2018");
+    getMissionMetaData.setStart(startDate);
+    getMissionMetaData.setEnd(endDate);
     GetMissionMetaDataRestV2Response response = (GetMissionMetaDataRestV2Response) getWebServiceTemplate()
       .marshalSendAndReceive(getMissionMetaData, new SoapActionCallback("Unicorn/GetMissionMetaDataRestV2"));
 
